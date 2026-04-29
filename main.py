f6d85b4dcfa9fb2269136eb7318e346d2fd8b3c6
Руди Герман Геннадьевич <rudige@yandex.ru>
2026-04-29 16:44:51 +0700

    feat: add parser mode routes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from utils.parser import ShowRuntime
+from utils.parser import CliESR, ShowRuntime
 
 app = FastAPI()
 
@@ -17,19 +17,59 @@ async def index(request: Request):
         request=request,
         name="index.html",
         context={
-            "title": "VPP Observer",
+            "title": "Observer",
+            "mode": None
         },
     )
 
 
-@app.post("/parse", response_class=HTMLResponse)
+@app.get("/vpp-show-runtime", response_class=HTMLResponse)
+async def vpp_show_runtime_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "title": "VPP NGFW Observer",
+            "mode": "vpp_show_runtime",
+        },
+    )
+
+
+@app.post("/vpp-show-runtime/parse", response_class=HTMLResponse)
 async def show_runtime(request: Request, raw_text: str = Form(...)):
     return templates.TemplateResponse(
         request=request,
         name="index.html",
         context={
             "title": "VPP Observer",
+            "mode": "vpp_show_runtime",
             "raw_text": raw_text,
             "parsed_show_runtime": ShowRuntime(raw_text).parsing(),
+        },
+    )
+
+
+@app.get("/esr-cli", response_class=HTMLResponse)
+async def esr_cli_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "title": "ESR Observer",
+            "mode": "cli_esr",
+        },
+    )
+
+
+@app.post("/esr-cli/parse", response_class=HTMLResponse)
+async def cli_esr(request: Request, raw_text: str = Form(...)):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={
+            "title": "CLI ESR Observer",
+            "mode": "cli_esr",
+            "raw_text": raw_text,
+            "parsed_cli_esr": CliESR(raw_text).parsing(),
         },
     )
